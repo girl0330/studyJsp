9834cdcd36b816c1edc09daae9dc0e73b3d87fee
genius <girls0330@naver.com>
2024-03-26 22:18:37 +0900

    start jsp study

---
+++ src/main/java/com/march/studyjsp/StudyJspApplication.java
@@ -0,0 +1,13 @@
+package com.march.studyjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/march/studyjsp/StudyJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.march.studyjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
